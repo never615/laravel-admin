62a198a7f054b44086f0491ecd8c24fecd81b956
never615 <583531034@qq.com>
2018-05-22 19:05:58 +0800

    管理端错误提示显示优化

--- src/Exception/Handler.php
+++ src/Exception/Handler.php
@@ -26,7 +26,7 @@ class Handler
         $errors = new ViewErrorBag();
         $errors->put('exception', $error);
 
-        \Log::error($exception);
+        \Log::warning($exception);
 
         return view('admin::partials.exception', compact('errors'))->render();
     }
