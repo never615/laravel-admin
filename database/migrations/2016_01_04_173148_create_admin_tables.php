a6b1f26663119fc0e2ed7583d38e6491bd9fa37a
never615 <583531034@qq.com>
2019-04-08 16:42:54 +0800

    移除role表name的unique约束,因为添加了subject_id,name 的约束

--- database/migrations/2016_01_04_173148_create_admin_tables.php
+++ database/migrations/2016_01_04_173148_create_admin_tables.php
@@ -26,7 +26,7 @@ class CreateAdminTables extends Migration
 
         Schema::connection($connection)->create(config('admin.database.roles_table'), function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 50)->unique();
+            $table->string('name', 50);
             $table->string('slug', 50);
             $table->timestamps();
         });
